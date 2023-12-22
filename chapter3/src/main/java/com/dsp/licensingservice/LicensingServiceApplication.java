2af06558b8480c852048a336346f0e79ba465e1d
sivan <sivananaintha.perumal@gmail.com>
2023-12-21 22:31:26 -0500

    changes to enable eureka client

--- chapter3/src/main/java/com/dsp/licensingservice/LicensingServiceApplication.java
+++ chapter3/src/main/java/com/dsp/licensingservice/LicensingServiceApplication.java
@@ -3,6 +3,7 @@ package com.dsp.licensingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
@@ -12,6 +13,7 @@ import java.util.Locale;
 
 @SpringBootApplication
 @RefreshScope
+@EnableEurekaClient
 public class LicensingServiceApplication {
 
 	public static void main(String[] args) {
