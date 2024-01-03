4c53c1093fe80f59a5d1218b12e43dd035792807
sivan <sivananaintha.perumal@gmail.com>
2024-01-03 16:02:14 -0500

    changes with producer and consumer changes working

--- licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -4,20 +4,17 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
-import com.optimagrowth.license.events.model.OrganizationChangeModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
@@ -29,7 +26,6 @@ import com.optimagrowth.license.utils.UserContextInterceptor;
 @RefreshScope
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableBinding(Sink.class)
 @EnableEurekaClient
 public class LicenseServiceApplication {
 
@@ -37,6 +33,11 @@ public class LicenseServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LicenseServiceApplication.class, args);
 	}
+
+//	@StreamListener(Sink.INPUT)
+//	public void loggerSink(OrganizationChangeModel orgChange) {
+//		logger.debug("Received {} event for the organization id {}", orgChange.getAction(), orgChange.getOrganizationId());
+//	}
 
 	@Bean
 	public LocaleResolver localeResolver() {
@@ -52,26 +53,20 @@ public class LicenseServiceApplication {
 		return messageSource;
 	}
 
-	@SuppressWarnings("unchecked")
-	@LoadBalanced
+	@Primary
 	@Bean
-	public RestTemplate getRestTemplate(){
+	public RestTemplate getCustomRestTemplate() {
 		RestTemplate template = new RestTemplate();
-        List interceptors = template.getInterceptors();
-        if (interceptors==null){
-            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
-        }
-        else{
-            interceptors.add(new UserContextInterceptor());
-            template.setInterceptors(interceptors);
-        }
+		List interceptors = template.getInterceptors();
+		if (interceptors == null) {
+			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		} else {
+			interceptors.add(new UserContextInterceptor());
+			template.setInterceptors(interceptors);
+		}
 
-        return template;
+		return template;
 	}
 
-	@StreamListener(Sink.INPUT)
-	public void loggerSink(OrganizationChangeModel orgChange) {
-		logger.debug("Received an {} event for organization id {}",
-				orgChange.getAction(), orgChange.getOrganizationId());
-	}
+
 }
